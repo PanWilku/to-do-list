ec50cf800442709072cb92a516319e36d8de9794
Marcin Wilk <marcin.wilk.503@gmail.com>
2025-02-03 07:34:42 +0900

    named modules changed to default modules

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,4 +1,4 @@
-export default class Task {
+export class Task {
     constructor(task, name) {
         this.task = task;
         this.name = name;
--- src/models/Task.js
+++ src/models/Task.js
@@ -1,4 +1,4 @@
-export default class Task {
+export class Task {
     constructor(title, description, dueDate, priority, notes, checkList) {
         this.id = Date.now();
         this.title = title;
