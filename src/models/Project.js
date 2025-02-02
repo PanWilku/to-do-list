0d3c069c26a9bb6fc06bebeaa47b1b6f96ebbe6b
Marcin Wilk <marcin.wilk.503@gmail.com>
2025-02-03 07:14:16 +0900

    task class initialized

---
+++ src/models/Project.js
@@ -0,0 +1,19 @@
+export default class Task {
+    constructor(task, name) {
+        this.task = task;
+        this.name = name;
+    }
+
+
+    addTask(task) {
+
+    };
+
+    removeTask(task) {
+
+    };
+
+    updateTask(task) {
+
+    };
+}
